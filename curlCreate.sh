34bfb90adacb5919a4b746f9670e1ddbd4a3e9a3
Sonsoles López Pernas <sonsoleslp@hotmail.com>
2018-08-30 14:08:34 +0200

    Add entity

---
+++ curlCreate.sh
@@ -0,0 +1,15 @@
+curl localhost:1026/v2/entities -s -S -H 'Content-Type: application/json' -d @- <<EOF
+{
+  "id": "Room1",
+  "type": "Room",
+  "temperature": {
+    "value": 23,
+    "type": "Float"
+  },
+  "pressure": {
+    "value": 720,
+    "type": "Integer"
+  }
+}
+EOF
+
